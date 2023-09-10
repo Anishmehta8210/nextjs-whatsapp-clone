6006de62b9b59254bcab68dc28c91fe39f003fb7
Anishmehta8210 <anishmehta7782@gmail.com>
2023-09-10 23:02:47 +0530

    2nd day

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,7 +1,43 @@
+"use client"
+import { UserButton } from "@clerk/nextjs";
+import { StreamChat} from "stream-chat";
+import {Channel, ChannelHeader, Chat, MessageInput, MessageList, Thread, Window} from "stream-chat-react"
+
+
+const userId = "user_2VBHq5vtKlssJqqBObPytzR3EpG";
+
+const chatClient = StreamChat.getInstance(
+  process.env.NEXT_PUBLIC_STREAM_KEY!
+)
+
+chatClient.connectUser(
+  {
+    id:userId,
+    name:"Anish Anand"
+  },
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidXNlcl8yVkJIcTV2dEtsc3NKcXFCT2JQeXR6UjNFcEcifQ.3aNXGn29cOnpT-7LMXueI7fbnpLWqetSIIqbUuK-9Gk"
+)
+
+const channel = chatClient.channel("messaging","channe_1",{
+  name:"Channel #1",
+  members:[userId],
+})
 export default function ChatPage() {
   return (
     <>
-      <h1>This is chat page</h1>
+      <Chat client={chatClient}>
+      <Channel channel={channel}>
+        <Window>
+          <ChannelHeader/>
+          <MessageList/>
+          <MessageInput/>
+        </Window>
+        <Thread/>
+
+      </Channel>
+        
+
+      </Chat>
     </>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Button from '@/components/Button'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -8,7 +9,7 @@ export default function Home() {
       <h1 className='text-6xl font-extrabold text-blue-500 mb-1'>Whatsapp Chat</h1>
 
       <p className='mb-10'>The Coolest Chat App</p>
-      <Button>Start Chating</Button>
+      <Button as={Link} href="/chat">Start Chating</Button>
        
     </div>
     </>
